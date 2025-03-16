1d58a78837746c0c9222bb3507ec6851a040ebe7
guilhermesecundo <guilhermesecundo45@gmail.com>
2025-03-15 23:36:57 -0300

    refatora main

--- src/main.py
+++ src/main.py
@@ -38,19 +38,7 @@ def main():
             processed_image = "processed_output.jpg"  
             ascii_output = "output_ascii.txt" 
 
-        print("Processing the image...")
-        process_image(input_image, processed_image, max_width=20)  
-
-        print("Converting the processed image into ASCII art...")
-        converter = ASCIIConverter()
-        ascii_art = converter.image_to_ascii(processed_image)  
-
-        print("Saving the ASCII art...")
-        converter.save_ascii_art(ascii_art, ascii_output)
-
-        print(f"Test completed! ASCII art saved in {ascii_output}")   
-    
-
+        
         print("Processing the image...")
         process_image(input_image, processed_image, max_width=350)  
 
@@ -65,4 +53,3 @@ def main():
 if __name__ == "__main__":
     main()
     
-#funcinando
